1d3646cb75a1d354e38c2527bbdf09338d037e3c
Mariel Martinez <mcortez.martinez@gmail.com>
2019-04-21 14:39:12 -0400

    Updates

---
+++ precache-manifest.d173e25546cc2dc52ad0f1b8632a0a17.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "dfc80616cbb7c746fb76",
+    "url": "/static/js/main.dfc80616.chunk.js"
+  },
+  {
+    "revision": "e883884898245765bc1b",
+    "url": "/static/js/2.e8838848.chunk.js"
+  },
+  {
+    "revision": "a87326baa82caff33a43e7ab783b880a",
+    "url": "/index.html"
+  }
+];
